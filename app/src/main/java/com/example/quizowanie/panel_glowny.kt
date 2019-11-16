4bce0f0d378822ec3913a76ec389438b88c8735e
Krzysiek <k.krup98@gmail.com>
2019-11-16 21:18:44 +0100

    Test

---
+++ app/src/main/java/com/example/quizowanie/panel_glowny.kt
@@ -0,0 +1,12 @@
+package com.example.quizowanie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class panel_glowny : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_panel_glowny)
+    }
+}
---
+++ app/src/main/java/com/example/quizowanie/panel_tworzenia.kt
@@ -0,0 +1,12 @@
+package com.example.quizowanie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class panel_tworzenia : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_panel_tworzenia)
+    }
+}
