fa673e62244b67c99876241c28c6d2d4393f4d2f
Krzysiek <k.krup98@gmail.com>
2019-11-25 22:32:31 +0100

    zmianyy x2

--- app/src/main/java/com/example/quizowanie/MainActivity.kt
+++ app/src/main/java/com/example/quizowanie/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.quizowanie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(applicationContext, panel_glowny::class.java))
+
+
+
+
+        val panelQuiz = findViewById<Button>(R.id.logowanieBT)
+        panelQuiz.setOnClickListener {
+            startActivity(Intent(applicationContext, panel_glowny::class.java))
+        }
     }
+
+
 }
--- app/src/main/java/com/example/quizowanie/panel_glowny.kt
+++ app/src/main/java/com/example/quizowanie/panel_glowny.kt
@@ -1,12 +1,151 @@
 package com.example.quizowanie
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class panel_glowny : AppCompatActivity() {
+
+    private var txtHighScore: TextView? = null
+    private var mhighscore: Int = 0
+    private var allscore: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_panel_glowny)
+
+
+        txtHighScore = this.findViewById(R.id.highscore)
+        loadHighScore()
+
+        val startQuiz = findViewById<Button>(R.id.pierwszyButton)
+        startQuiz.setOnClickListener {
+            startActivityForResult(Intent(applicationContext, MainQuiz::class.java), REQUEST_CODE)
+        }
+      /*  if (allscore > 76) {
+            val startQuiz2 = findViewById<Button>(R.id.drugiButton)
+            startQuiz2.setOnClickListener {
+                startActivityForResult(
+                    Intent(applicationContext, DrugiQuiz::class.java),
+                    REQUEST_CODE
+                )
+
+            }
+        } else {
+            val startQuiz3 = findViewById<Button>(R.id.drugiButton)
+            startQuiz3.setOnClickListener {
+                Toast.makeText(
+                    this,
+                    "Masz za mało punktów żeby otworzyć tą kategorie\n-Potrzeba 70 pkt",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+*/
     }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        if (allscore > 89) {
+            val startQuiz2 = findViewById<Button>(R.id.drugiButton)
+            startQuiz2.setOnClickListener {
+                startActivityForResult(
+                    Intent(applicationContext, DrugiQuiz::class.java),
+                    REQUEST_CODE
+                )
+
+            }
+        } else {
+            val startQuiz3 = findViewById<Button>(R.id.drugiButton)
+            startQuiz3.setOnClickListener {
+                Toast.makeText(
+                    this,
+                    "Masz za mało punktów żeby otworzyć tą kategorie\n-Potrzeba 90 pkt",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
+        if (allscore > 179) {
+            val startQuiz2 = findViewById<Button>(R.id.trzeciButton)
+            startQuiz2.setOnClickListener {
+                startActivityForResult(
+                    Intent(applicationContext, TrzeciQuiz::class.java),
+                    REQUEST_CODE
+                )
+
+            }
+        } else {
+            val startQuiz3 = findViewById<Button>(R.id.trzeciButton)
+            startQuiz3.setOnClickListener {
+                Toast.makeText(
+                    this,
+                    "Masz za mało punktów żeby otworzyć tą kategorie\n-Potrzeba 180 pkt",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val score = data!!.getIntExtra(MainQuiz.FINAL_SCORE, 0)
+                allscore = score + allscore
+                txtHighScore!!.text = "Wszystkie punkty:- $allscore"
+                val preferences = getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+                val editor = preferences.edit()
+                editor.putInt(HIGH_SCORE, allscore)
+                editor.apply()
+                    //   if (score > mhighscore) {
+                 //   updateScore(score)
+               // }
+            }
+        }
+
+    }
+
+   /* private fun updateScore(score: Int) {
+        mhighscore = score
+        txtHighScore!!.text = "My High Score:- $mhighscore"
+
+        val preferences = getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putInt(HIGH_SCORE, mhighscore)
+        editor.apply()
+
+        }
+*/
+
+    private fun loadHighScore() {
+        val preferences = getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+        allscore = preferences.getInt(HIGH_SCORE, 0)
+        txtHighScore!!.text = "Punkty:- $allscore"
+    }
+   // private fun loadHighScore() {
+   //     val preferences = getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+    //    mhighscore = preferences.getInt(HIGH_SCORE, 0)
+   //     txtHighScore!!.text = "My High Score:- $mhighscore"
+
+  //  }
+    companion object {
+
+        private val REQUEST_CODE = 1
+        val PREFS = "shared_prefs"
+        val HIGH_SCORE = "high_score"
+    }
+
 }
